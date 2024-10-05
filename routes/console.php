327e63b934baa317eb9b63e1120abc4df98abc16
adil shah <adilkhan12324@gmail.com>
2024-10-05 18:58:03 +0500

    add feature of task schedular for every five minutes. add API-documentation-swagger-editor.yml file for API documentation,use this yml code in the swagger editor for api specifications. Add steps in the README.md file so it will help to run the project.

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(StoreNewsApis::class)->everyMinute();
+Schedule::command(StoreNewsApis::class)->everyFiveMinutes();
